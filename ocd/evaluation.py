f610f2a04b4f1278826470651fa12344823d6907
hamidreza <hamidrezakamkari@gmail.com>
2022-12-08 10:20:17 -0500

    add backward score backward/all

--- ocd/evaluation.py
+++ ocd/evaluation.py
@@ -3,6 +3,7 @@ import typing as th
 from ocd.data import generate_datasets
 import numpy as np
 
+
 def count_backward(perm: th.List[int], dag: np.array):
     """
     Args: 
@@ -21,12 +22,35 @@ def count_backward(perm: th.List[int], dag: np.array):
                 count += 1
     return count
 
+
+def backward_score(perm: th.List[int], dag: np.array):
+    """
+    Args: 
+        perm (list): permutation of the nodes
+        dag (np.array): adjacency matrix of the DAG
+
+    Compute the ratio of backward edges to all edges given a permutation
+
+    This is used as a validation metric
+    """
+    n = len(perm)
+    backwards, all = 0, 0
+    for i in range(n):
+        for j in range(n):
+            if dag[perm[j], perm[i]]:
+                all += 1
+                if i < j:
+                    backwards += 1
+    return 1.0 * backwards / all
+
+
 def shd(dag1: np.array, dag2: np.array):
     """
     Compute the structural hamming distance (SHD) between two DAGs
     """
     return np.sum(dag1 != dag2)
-    
+
+
 def closure_distance(perm: th.List[int], dag: np.array):
     """
     Compare the closure of the DAG with the tournament induced by the permutation
@@ -39,14 +63,10 @@ def closure_distance(perm: th.List[int], dag: np.array):
             perm_tournament[perm[i], perm[j]] = 1
     return shd(dag_closure, perm_tournament)
 
+
 def closure(dag: np.array):
     for k in range(len(dag)):
         for i in range(len(dag)):
             for j in range(len(dag)):
                 dag[i, j] = dag[i, j] or (dag[i, k] and dag[k, j])
     return dag
-
-
-
-
-    
