a90e90bc19e90ca096e7e5da55a556d6675163bf
vahidzee <vahid98zee@gmail.com>
2022-12-09 20:24:05 -0500

    add linear feature embedding

--- ocd/training/module.py
+++ ocd/training/module.py
@@ -14,7 +14,12 @@ class OrderedTrainingModule(TrainingModule):
         # model
         in_covariate_features: th.List[int],
         hidden_features_per_covariate: th.List[th.List[int]],
-        bias: bool = True,
+        # data transform
+        embedding_dim: int = 0,  # 0 for one-hot encoding
+        embedding_normalization: th.Optional[th.Union[int, str]] = None,  # None for no normalization,
+        embedding_normalization_eps: float = 0,
+        # architecture
+        bias: bool = False,
         activation: th.Optional[str] = "torch.nn.ReLU",
         activation_args: th.Optional[dict] = None,
         batch_norm: bool = False,
@@ -27,8 +32,7 @@ class OrderedTrainingModule(TrainingModule):
         criterion_args: th.Optional[dict] = None,
         # optimization configs [is_active(training_module, optimizer_idx) -> bool]
         optimizer: th.Union[str, th.List[str]] = "torch.optim.Adam",
-        optimizer_is_active: th.Optional[th.Union[dy.FunctionDescriptor,
-                                                  th.List[dy.FunctionDescriptor]]] = None,
+        optimizer_is_active: th.Optional[th.Union[dy.FunctionDescriptor, th.List[dy.FunctionDescriptor]]] = None,
         optimizer_parameters: th.Optional[th.Union[th.List[str], str]] = None,
         optimizer_args: th.Optional[dict] = None,
         # learning rate
@@ -42,14 +46,17 @@ class OrderedTrainingModule(TrainingModule):
         scheduler_frequency: th.Union[int, th.List[int]] = 1,
         scheduler_monitor: th.Optional[th.Union[str, th.List[str]]] = None,
     ):
+        # input stats
+        self.in_covariate_features = in_covariate_features
         # initialize model and optimizer/scheduler configs
-        _criterion_args = dict(
-            terms=["ocd.training.terms.OrderedLikelihoodTerm"])
+        _criterion_args = dict(terms=["ocd.training.terms.OrderedLikelihoodTerm"])
         _criterion_args.update(criterion_args or {})
         super().__init__(
             model_cls="ocd.models.order_discovery.SinkhornOrderDiscovery",
             model_args=dict(
-                in_covariate_features=in_covariate_features,
+                in_covariate_features=(
+                    in_covariate_features if not embedding_dim else [embedding_dim] * len(in_covariate_features)
+                ),
                 hidden_features_per_covariate=hidden_features_per_covariate,
                 bias=bias,
                 activation=activation,
@@ -78,6 +85,28 @@ class OrderedTrainingModule(TrainingModule):
             # hparams
             save_hparams=True,
         )
+        # embedding
+        self.features_embedding = (
+            torch.nn.Embedding(
+                num_embeddings=len(in_covariate_features),
+                embedding_dim=embedding_dim,
+            )
+            if embedding_dim
+            else None
+        )
+        self.embeddings = (
+            torch.nn.Embedding(
+                num_embeddings=sum(in_covariate_features),
+                embedding_dim=embedding_dim,
+            )
+            if embedding_dim
+            else None
+        )
+        self.embedding_normalization = embedding_normalization
+        self.embedding_normalization_eps = embedding_normalization_eps
+        # register cummulative feature counts (for efficient embedding)
+        self.register_buffer("features_cumsum", torch.cumsum(torch.tensor([0] + in_covariate_features[:-1]), dim=0))
+
         # set tau scheduler
         self.__tau_scheduler = tau_scheduler  # processed in self.tau_scheduler
         # processed in self.n_sinkhorn_scheduler
@@ -108,40 +137,59 @@ class OrderedTrainingModule(TrainingModule):
         # for binary covariates it should be batch_size * (num_covariates * 2)
 
         # the number of classes for each covariate is set in
-        # self.model.in_covariate_features and the number of covariates is the length of this list
+        # self.in_covariate_features and the number of covariates is the length of this list
 
         if not transform_batch:
             return transformed_batch if transformed_batch is not None else batch
 
-        interventional_batch_size = batch[1].shape[0] if isinstance(
-            batch, (list, tuple)) else 0
+        interventional_batch_size = batch[1].shape[0] if isinstance(batch, (list, tuple)) else 0
 
         if isinstance(batch, (list, tuple)):
             # concat the batches in the list in the first dimension
             batch = torch.cat(batch, dim=0)
 
-        # transform batch
-        # apply one hot encoding to each covariate
-        # batch_size * num_covariates -> batchsize * sum(num_classes_per_covariate)
-        num_classes_per_covariate = self.model.in_covariate_features
+        # compute batch stats (sizes and dimensions)
+        num_classes_per_covariate = self.in_covariate_features
         batch_size = batch.shape[0]
         num_covariates = len(num_classes_per_covariate)
         num_classes = sum(num_classes_per_covariate)
-        transformed_batch = torch.zeros(
-            batch_size, num_classes, device=batch.device)
-        start_idx = 0
-        for i in range(num_covariates):
-            end_idx = start_idx + num_classes_per_covariate[i]
-            transformed_batch[:, start_idx:end_idx] = torch.nn.functional.one_hot(
-                batch[:, i], num_classes=num_classes_per_covariate[i]
+
+        # compute the embeddings (one-hot or linear)
+        if self.features_embedding is not None:
+            # linearly embed the covariates each covariate embedding is = value embedding  + feature embedding
+            # where we have the same number of features as covariates, and the same number of values as the sum of the number of classes per covariate
+            features_embeddings = self.features_embedding(
+                torch.arange(len(self.in_covariate_features), device=batch.device)
             )
-            start_idx = end_idx
+            value_embeddings = self.embeddings(batch + self.features_cumsum)
+            # normalize the embeddings
+            if self.embedding_normalization is not None:
+                features_embeddings = features_embeddings / (
+                    features_embeddings.norm(p=self.embedding_normalization, dim=1, keepdim=True)
+                    + self.embedding_normalization_eps
+                )
+                value_embeddings = value_embeddings / (
+                    value_embeddings.norm(p=self.embedding_normalization, dim=1, keepdim=True)
+                    + self.embedding_normalization_eps
+                )
+            transformed_batch = (value_embeddings + features_embeddings).reshape(batch_size, -1)
+        else:
+            # apply one hot encoding to each covariate
+            # batch_size * num_covariates -> batchsize * sum(num_classes_per_covariate)
+            transformed_batch = torch.zeros(batch_size, num_classes, device=batch.device)
+            start_idx = 0
+            for i in range(num_covariates):
+                end_idx = start_idx + num_classes_per_covariate[i]
+                transformed_batch[:, start_idx:end_idx] = torch.nn.functional.one_hot(
+                    batch[:, i], num_classes=num_classes_per_covariate[i]
+                )
+                start_idx = end_idx
 
         if interventional_batch_size > 0:
             # split the batch into observational and interventional
             transformed_batch = [
                 transformed_batch[: batch_size - interventional_batch_size],
-                transformed_batch[batch_size - interventional_batch_size:],
+                transformed_batch[batch_size - interventional_batch_size :],
             ]
 
         return transformed_batch
@@ -185,23 +233,19 @@ class OrderedTrainingModule(TrainingModule):
             permutation = derive_deterministic_permutation(self.model.Gamma)
             # compare to the true graph structure
             # get datamodule
-            self.log("metrics/tau", self.model.tau,
-                     on_step=False, on_epoch=True)
-            self.log("metrics/n_iter", self.model.n_iter,
-                     on_step=False, on_epoch=True)
+            self.log("metrics/tau", self.model.tau, on_step=False, on_epoch=True)
+            self.log("metrics/n_iter", self.model.n_iter, on_step=False, on_epoch=True)
             self.log(
                 "metrics/backwards_count",
-                count_backward(
-                    permutation, self.trainer.datamodule.datasets[0].dag),
+                count_backward(permutation, self.trainer.datamodule.datasets[0].dag),
                 on_step=False,
                 on_epoch=True,
                 prog_bar=True,
                 logger=True,
             )
             self.log(
-                "metrics/backwards_count_divided_by_num_edges",
-                backward_score(
-                    permutation, self.trainer.datamodule.datasets[0].dag),
+                "metrics/normalized_backwards_count",
+                backward_score(permutation, self.trainer.datamodule.datasets[0].dag),
                 on_step=False,
                 on_epoch=True,
                 prog_bar=True,
