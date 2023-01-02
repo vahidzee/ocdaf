7cff942999d7414c289158e4da496bd79a9f0371
Vahid Zehtab <33608325+vahidzee@users.noreply.github.com>
2023-01-02 15:16:31 -0500

    remove legacy permutation, utils, made codes

--- ocd/evaluation.py
+++ ocd/evaluation.py
@@ -1,11 +1,32 @@
-
 import typing as th
 import numpy as np
+import random
+
+
+def dfs(node, adj, stack, visited):
+    visited[node] = True
+    # Create a permuted list of indices from 0 to len(adj)
+    idx = list(range(len(adj)))
+    random.shuffle(idx)
+    for i in idx:
+        if adj[node][i] and not visited[i]:
+            dfs(i, adj, stack, visited)
+    stack.append(node)
+
+
+def topological_sort(adj):
+    stack = []
+    visited = [False] * len(adj)
+    for i in range(len(adj)):
+        if not visited[i]:
+            dfs(i, adj, stack, visited)
+    # invert the stack
+    return stack[::-1]
 
 
 def count_backward(perm: th.List[int], dag: np.array):
     """
-    Args: 
+    Args:
         perm (list): permutation of the nodes
         dag (np.array): adjacency matrix of the DAG
 
@@ -16,7 +37,7 @@ def count_backward(perm: th.List[int], dag: np.array):
     n = len(perm)
     count = 0
     for i in range(n):
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             if dag[perm[j], perm[i]]:
                 count += 1
     return count
@@ -24,7 +45,7 @@ def count_backward(perm: th.List[int], dag: np.array):
 
 def backward_score(perm: th.List[int], dag: np.array):
     """
-    Args: 
+    Args:
         perm (list): permutation of the nodes
         dag (np.array): adjacency matrix of the DAG
 
@@ -58,7 +79,7 @@ def closure_distance(perm: th.List[int], dag: np.array):
     dag_closure = closure(dag)
     perm_tournament = np.zeros((n, n))
     for i in range(n):
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             perm_tournament[perm[i], perm[j]] = 1
     return shd(dag_closure, perm_tournament)
 
