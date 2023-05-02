bf89be312847657022e24a010a99e9ef754dabac
Vahid Zehtab <33608325+vahidzee@users.noreply.github.com>
2023-05-01 21:25:45 -0400

    update cam baseline

--- baselines/CAM.py
+++ baselines/CAM.py
@@ -1,21 +1,25 @@
-from baselines.baseline import Baseline
-from ocd.data import OCDDataset
+from base import AbstractBaseline
 from cdt.causality.graph import CAM as CDT_CAM
 import networkx as nx
+import typing as th
 
 
-class CAM(Baseline):
-    def __init__(self):
-        super().__init__("CAM")
+class CAM(AbstractBaseline):
+    """CAM baseline from CDT"""
 
-    def fit(self, dataset: OCDDataset, linear: bool = False):
-        """ Fit the baseline on the dataset
-        Args:
-            dataset: The dataset to fit on
-            linear: If the data is linear
-        """
-        samples = dataset.samples
-        score = 'linear' if linear else 'nonlinear'
-        graph = CDT_CAM(score=score, pruning=False).predict(samples)
+    def __init__(
+        self,
+        dataset: th.Union["OCDDataset", str],  # type: ignore
+        name: th.Optional[str] = None,  # name to override the default name
+        dataset_args: th.Optional[th.Dict[str, th.Any]] = None,
+        # hyperparameters
+        linear: bool = False,
+    ):
+        super().__init__(dataset=dataset, dataset_args=dataset_args, name=name)
+        self.linear = linear
+
+    def estimate_order(self):
+        samples = self.get_data(conversion="pandas")
+        graph = CDT_CAM(score="linear" if self.linear else "nonlinear", pruning=False).predict(samples)
         orders = list(nx.topological_sort(graph))
         return orders
