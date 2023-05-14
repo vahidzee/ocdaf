d195f43fd08958874a4fa4a466dde410b479bc85
vdblm <balazadehvahid@gmail.com>
2023-05-13 23:05:34 -0400

    cam pruning

--- ocd/post_processing/cam_pruning.py
+++ ocd/post_processing/cam_pruning.py
@@ -1,3 +1,8 @@
+# Codes are adopted from the original implementation
+# https://github.com/paulrolland1307/SCORE/tree/5e18c73a467428d51486d2f683349dde2607bfe1
+# under the GNU Affero General Public License v3.0
+# Copy right belongs to the original author https://github.com/paulrolland1307
+
 import os
 import uuid
 
