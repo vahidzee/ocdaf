31c62a4a3be8384d920f9952a7ac9acbd2add59b
Vahid Zehtab <vahid98zee@gmail.com>
2023-05-14 15:28:49 -0400

    fix intervention callback mislabels

--- ocd/training/callbacks/intervention.py
+++ ocd/training/callbacks/intervention.py
@@ -12,9 +12,8 @@ class InterventionCallback(pl.Callback):
         self,
         every_n_epochs: int = 1,
         flow_path: str = "model.flow",
-        bins: int = 100,
-        num_samples: int = 200,
-        num_interventions: int = 50,
+        num_samples: int = 100,
+        num_interventions: int = 250,
         k: int = 5,
         target: int = -1,
         percentile: float = 0.9,
@@ -23,7 +22,6 @@ class InterventionCallback(pl.Callback):
         super().__init__()
         self.every_n_epochs = every_n_epochs
         self.flow_path = flow_path
-        self.bins = bins
         self.num_samples = num_samples
         self.num_interventions = num_interventions
         self.k = k
@@ -57,7 +55,7 @@ class InterventionCallback(pl.Callback):
             plt.xlim(-self.k -1, self.k + 1)
             plt.xlabel("$do(X_1)$")
             tar = (self.data.data.shape[-1] + self.target) if (self.target < 0) else self.target
-            plt.plot(values, data_means, label="Data", color="blue")
+            plt.plot(values, data_means, label="True Density", color="blue")
             plt.fill_between(
                 values, (data_means - 3 * data_stds), (data_means + 3 * data_stds), alpha=0.2, color="blue"
             )
@@ -65,8 +63,8 @@ class InterventionCallback(pl.Callback):
             if self.limit_y > 0:
                 plt.ylim(abs(original_limits[0]) * (-1 - self.limit_y), abs(original_limits[1]) * (1 + self.limit_y))
 
-            plt.ylabel("$\mathbb{E}_{X_1\sim Uniform(" + str(-self.k) + "," + str(self.k) + ")}[X_{" + str(tar + 1) + "} | do(X_1)]$")
-            plt.plot(values, model_means, label="Model", color="red")
+            plt.ylabel("$P(X_{" + str(tar + 1) + "} | do(X_1))$")
+            plt.plot(values, model_means, label="Predicted Density", color="red")
             plt.fill_between(
                 values, (model_means - 3 * model_stds), (model_means + 3 * model_stds), alpha=0.2, color="red"
             )
@@ -92,5 +90,5 @@ class InterventionCallback(pl.Callback):
             image_flat = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')  # (H * W * 3,)
             plot = image_flat.reshape(*fig.canvas.get_width_height(), 3)  # (H, W, 3)
 
-        trainer.logger.log_image("Data Fit", images=[plot], caption=["intervention"])
+        trainer.logger.log_image("Interventions", images=[plot], caption=["intervention"])
         return return_value
