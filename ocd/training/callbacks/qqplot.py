57b2a4f7cd86c195ec50869758bf20c63e05d0c8
Vahid Zehtab <vahid98zee@gmail.com>
2023-05-14 13:16:24 -0400

    fix qqplot intervention callback

--- ocd/training/callbacks/qqplot.py
+++ ocd/training/callbacks/qqplot.py
@@ -5,18 +5,17 @@ from matplotlib import pyplot as plt
 import torch
 import numpy as np
 import dypy as dy
-
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 def qqplot(x: torch.Tensor, y: torch.Tensor, bins=200, alpha=0.5, x_label="x", y_label="y"):
     # x and y are tensors of shape (N, D)
     # we want to plot the qqplot for i==j to get a grid of (1 x D) plots
-    assert x.shape == y.shape
+    # assert x.shape == y.shape # number of samples could be different
     d = x.shape[-1]
     fig, axs = plt.subplots(d, figsize=(20, 20))
     for i in range(d):
-        axs[i].hist(x[:, i], bins=bins, alpha=alpha, label=x_label, density=True, color="blue")
-        # draw the kdensity plot
-        axs[i].hist(y[:, i], bins=bins, alpha=alpha, label=y_label, density=True, color="red")
+        axs[i].hist(x[:, i].flatten(), bins=bins, alpha=alpha, label=x_label, density=True, color="blue")
+        axs[i].hist(y[:, i].flatten(), bins=bins, alpha=alpha, label=y_label, density=True, color="red")
         axs[i].legend()
     return fig
 
@@ -24,7 +23,7 @@ def qqplot(x: torch.Tensor, y: torch.Tensor, bins=200, alpha=0.5, x_label="x", y
 class QQplotCallback(pl.Callback):
     def __init__(
         self,
-        every_n_epochs: int = 1,
+        every_n_epochs: int = 5,
         flow_path: str = "model.flow",
         bins: int = 100,
         num_samples: int = 1000,
@@ -58,7 +57,13 @@ class QQplotCallback(pl.Callback):
             model_samples = self.flow.sample(self.num_samples).detach().cpu()
             data_samples = self.data.data.detach().cpu()
             fig = qqplot(data_samples, model_samples, bins=self.bins, x_label="data", y_label="model")
-            plot = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
-            plot = plot.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-        trainer.logger.log_image("Data", images=[plot], caption=["qqplot"])
+
+            canvas = FigureCanvas(fig)
+            canvas.draw()       # draw the canvas, cache the renderer
+
+            image_flat = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')  # (H * W * 3,)
+            plot = image_flat.reshape(*fig.canvas.get_width_height(), 3)  # (H, W, 3)
+
+
+        trainer.logger.log_image("Data Fit", images=[plot], caption=["qqplot"])
         return return_value
