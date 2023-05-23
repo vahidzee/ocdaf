8e60a59c17b3c9932bf2b5618752d330de80da03
Vahid Zehtab <33608325+vahidzee@users.noreply.github.com>
2023-05-23 19:28:03 -0400

    add grid visualization callback

--- ocd/training/callbacks/intervention.py
+++ ocd/training/callbacks/intervention.py
@@ -7,6 +7,173 @@ import numpy as np
 import dypy as dy
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
+plt.rcParams.update({"font.size": 18})
+
+
+def _draw_ax(
+    ax,
+    n,
+    k,
+    values=None,
+    pred_means=None,
+    pred_stds=None,
+    gt_means=None,
+    gt_stds=None,
+    target=-1,
+    percentile=0.95,
+    limit_y=0.9,
+    limit_ys=None,
+    icis=None,
+):
+    if pred_means is not None:
+        pred_mean = pred_means[:, target]
+        pred_std = pred_stds[:, target]
+    if gt_means is not None:
+        gt_mean = gt_means[:, target]
+        gt_std = gt_stds[:, target]
+    ax.set_xlim(-k, k)
+    ax.set_xlabel("$do(X_1)$")
+    tar = (n + target) if (target < 0) else target
+    if gt_means is not None:
+        ax.plot(values, gt_mean, label="True Density", color="blue")
+        ax.fill_between(values, (gt_mean - 3 * gt_std), (gt_mean + 3 * gt_std), alpha=0.2, color="blue")
+    original_limits = ax.get_ylim()
+    if limit_ys is not None:
+        ax.set_ylim(limit_ys[0], limit_ys[1])
+    elif limit_y > 0:
+        ax.set_ylim(abs(original_limits[0]) * (-1 - limit_y), abs(original_limits[1]) * (1 + limit_y))
+
+    ax.set_ylabel("$P(X_{" + str(tar + 1) + "} | do(X_1))$")
+    if pred_means is not None:
+        ax.plot(values, pred_mean, label="Predicted Density", color="red")
+        ax.fill_between(values, (pred_mean - 3 * pred_std), (pred_mean + 3 * pred_std), alpha=0.2, color="red")
+    ax.legend(loc="lower right")
+    # plot the confidence intervals as two vertical lines
+    if percentile > 0:
+        ax.axvline(icis[0].item(), color="gray", linestyle="--")
+        ax.axvline(icis[1].item(), color="gray", linestyle="--")
+        # draw a horizontal line with two arrows pointing to the vertical lines
+        ax.annotate(
+            "",
+            xy=(icis[0].item(), ax.get_ylim()[1] - 1.25),
+            xytext=(icis[1].item(), ax.get_ylim()[1] - 1.25),
+            arrowprops=dict(arrowstyle="<->", color="gray"),
+        )
+        # label the confidence interval
+        ax.text(
+            (icis[0].item() + icis[1].item()) / 2,
+            ax.get_ylim()[1] - 1.75,
+            f"${percentile*100: 0.1f}$% CI for $X_1$",
+            horizontalalignment="center",
+            verticalalignment="center",
+            fontdict={"fontsize": 15},
+        )
+
+
+def draw_grid(
+    n,
+    k,
+    target=None,
+    values=None,
+    pred_means=None,
+    pred_stds=None,
+    gt_means=None,
+    gt_stds=None,
+    percentile=0.95,
+    limit_y=0.9,
+    limit_ys=None,
+    icis=None,
+):
+    targets = target if target is not None else list(range(n))
+    nrows = len(targets)
+    fig, axs = plt.subplots(1, nrows, figsize=(8 * nrows, 8))  # Adjust the figure size as necessary
+
+    for i, target in enumerate(targets):
+        _draw_ax(
+            ax=axs[i],
+            n=n,
+            k=k,
+            values=values,
+            pred_means=pred_means,
+            pred_stds=pred_stds,
+            gt_means=gt_means,
+            gt_stds=gt_stds,
+            target=target,
+            percentile=percentile,
+            limit_y=limit_y,
+            limit_ys=limit_ys,
+            icis=icis,
+        )
+    return fig
+
+
+def draw(
+    fig,
+    n,
+    k,
+    values=None,
+    pred_means=None,
+    pred_stds=None,
+    gt_means=None,
+    gt_stds=None,
+    target=-1,
+    percentile=0.95,
+    limit_y=0.9,
+    limit_ys=None,
+    icis=None,
+):
+    if pred_means is not None:
+        pred_mean = pred_means[:, target]
+        pred_std = pred_stds[:, target]
+    if gt_means is not None:
+        gt_mean = gt_means[:, target]
+        gt_std = gt_stds[:, target]
+    plt.xlim(-k, k)
+    plt.xlabel("$do(X_1)$")
+    tar = (n + target) if (target < 0) else target
+    if gt_means is not None:
+        plt.plot(values, gt_mean, label="True Density", color="blue")
+        plt.fill_between(values, (gt_mean - 3 * gt_std), (gt_mean + 3 * gt_std), alpha=0.2, color="blue")
+    original_limits = plt.ylim()
+    if limit_ys is not None:
+        plt.ylim(limit_ys[0], limit_ys[1])
+    elif limit_y > 0:
+        plt.ylim(abs(original_limits[0]) * (-1 - limit_y), abs(original_limits[1]) * (1 + limit_y))
+
+    plt.ylabel("$P(X_{" + str(tar + 1) + "} | do(X_1))$")
+    if pred_means is not None:
+        plt.plot(values, pred_mean, label="Predicted Density", color="red")
+        plt.fill_between(values, (pred_mean - 3 * pred_std), (pred_mean + 3 * pred_std), alpha=0.2, color="red")
+    plt.legend(loc="lower right")
+    # plt.grid()
+    # based on data.base_distribution compute the 90% confidence interval for values
+    # plot the confidence intervals as two vertical lines
+    if percentile > 0:
+        plt.axvline(icis[0].item(), color="gray", linestyle="--")
+        plt.axvline(icis[1].item(), color="gray", linestyle="--")
+        # label the confidence interval
+        # create a horizontal line with two arrows pointing to the vertical lines
+        plt.annotate(
+            "",
+            xy=(icis[0].item(), plt.ylim()[1] - 1.25),
+            xytext=(icis[1].item(), plt.ylim()[1] - 1.25),
+            arrowprops=dict(arrowstyle="<->", color="gray"),
+        )
+        # label the confidence interval
+        plt.text(
+            (icis[0].item() + icis[1].item()) / 2,
+            plt.ylim()[1] - 1.75,
+            f"${percentile*100: 0.1f}$% CI for $X_1$",
+            horizontalalignment="center",
+            verticalalignment="center",
+            fontdict={"fontsize": 15},
+        )
+    return fig
+
+    # set x ticks to have values at every 2.5 from zero to both sides
+    # plt.xticks((-torch.arange(0, k, 2.5)).tolist()[1:][::-1] + torch.arange(0, k, 2.5).tolist())
+
+
 class InterventionCallback(pl.Callback):
     def __init__(
         self,
@@ -15,9 +182,10 @@ class InterventionCallback(pl.Callback):
         num_samples: int = 100,
         num_interventions: int = 250,
         k: float = 5,
-        target: int = -1,
+        target: th.Union[int, th.List[int], None] = -1,
         percentile: float = 0.9,
-        limit_y: float = 0.9, # 0 means no limit
+        limit_y: float = 0.9,  # 0 means no limit
+        limit_ys: th.Optional[th.List[float]] = None,
     ):
         super().__init__()
         self.every_n_epochs = every_n_epochs
@@ -26,6 +194,7 @@ class InterventionCallback(pl.Callback):
         self.num_interventions = num_interventions
         self.k = k
         self.limit_y = limit_y
+        self.limit_ys = limit_ys
         self.target = target
         self.percentile = percentile
         self.flow = None  # read it on fit
@@ -42,53 +211,63 @@ class InterventionCallback(pl.Callback):
             if (trainer.current_epoch + 1) % self.every_n_epochs != 0:
                 return return_value
 
+        # data shape
+        n = self.data.data.shape[-1]
+
+        # precentiles
+        cis, icis = None, None
+        if self.percentile:
+            cis = [(1 - self.percentile) / 2, 1 - (1 - self.percentile) / 2]
+            icis = self.data.base_distribution.icdf(torch.tensor(cis)).detach().cpu()
+
+        # predictions
         with torch.no_grad():
             values = torch.linspace(-self.k, self.k, self.num_interventions)
-            model_samples = self.flow.do(idx=0, values=values, target=self.target, num_samples=self.num_samples).detach().cpu()
-            model_means = model_samples.mean(-1)
-            model_stds = model_samples.std(-1)
-            data_samples = self.data.do(idx=0, values=values, target=self.target, num_samples=self.num_samples).detach().cpu()
-            data_means = data_samples.mean(-1)
-            data_stds = data_samples.std(-1)
-            fig = plt.figure(figsize=(10, 10))
-            # plot ate vs values of do with std as confidence interval
-            plt.xlim(-self.k -1, self.k + 1)
-            plt.xlabel("$do(X_1)$")
-            tar = (self.data.data.shape[-1] + self.target) if (self.target < 0) else self.target
-            plt.plot(values, data_means, label="True Density", color="blue")
-            plt.fill_between(
-                values, (data_means - 3 * data_stds), (data_means + 3 * data_stds), alpha=0.2, color="blue"
+            pred_samples = self.flow.do(idx=0, values=values, num_samples=self.num_samples).detach().cpu()
+            pred_means = pred_samples.mean(-2)
+            pred_stds = pred_samples.std(-2)
+            gt_samples = self.data.do(idx=0, values=values, num_samples=self.num_samples).detach().cpu()
+            data_means = gt_samples.mean(-2)
+            data_stds = gt_samples.std(-2)
+
+        if self.target is None or isinstance(self.target, list):
+            draw_grid(
+                n=n,
+                k=self.k,
+                values=values,
+                pred_means=pred_means,
+                pred_stds=pred_stds,
+                gt_means=data_means,
+                gt_stds=data_stds,
+                target=self.target,
+                percentile=self.percentile,
+                limit_y=self.limit_y,
+                limit_ys=self.limit_ys,
+                icis=icis,
             )
-            original_limits = plt.ylim()
-            if self.limit_y > 0:
-                plt.ylim(abs(original_limits[0]) * (-1 - self.limit_y), abs(original_limits[1]) * (1 + self.limit_y))
-
-            plt.ylabel("$P(X_{" + str(tar + 1) + "} | do(X_1))$")
-            plt.plot(values, model_means, label="Predicted Density", color="red")
-            plt.fill_between(
-                values, (model_means - 3 * model_stds), (model_means + 3 * model_stds), alpha=0.2, color="red"
+        else:
+            fig = draw(
+                fig=plt.figure(figsize=(8, 8)),
+                n=n,
+                k=self.k,
+                values=values,
+                pred_means=pred_means,
+                pred_stds=pred_stds,
+                gt_means=data_means,
+                gt_stds=data_stds,
+                target=self.target,
+                percentile=self.percentile,
+                limit_y=self.limit_y,
+                limit_ys=self.limit_ys,
+                icis=icis,
             )
-            plt.legend()
-            plt.grid()
-            # based on self.data.base_distribution compute the 90% confidence interval for values
-            # plot the confidence intervals as two vertical lines
-            if self.percentile > 0:
-                cis = [(1 - self.percentile)/2, 1 - (1 - self.percentile)/2]
-                icis = self.data.base_distribution.icdf(torch.tensor(cis)).detach().cpu()
-                plt.axvline(icis[0].item(), color="gray", linestyle="--")
-                plt.axvline(icis[1].item(), color="gray", linestyle="--")
-                # label the confidence interval
-                plt.text(icis[0].item() + 0.1, plt.ylim()[1] -0.5, f"$X_1$ ${cis[0]*100: 0.1f}$% CI", rotation=90, verticalalignment="center", fontdict={"fontsize": 8})
-                plt.text(icis[1].item() + 0.1, plt.ylim()[1] -0.5, f"$X_1$ ${cis[1]*100: 0.1f}$% CI", rotation=90, verticalalignment="center", fontdict={"fontsize": 8})
 
-            plt.title(f"$N={self.data.data.shape[-1]}$")
-            
-            # convert the figure to a numpy array to be logged
-            canvas = FigureCanvas(fig)
-            canvas.draw()       # draw the canvas, cache the renderer
+        # convert the figure to a numpy array to be logged
+        canvas = FigureCanvas(fig)
+        canvas.draw()  # draw the canvas, cache the renderer
 
-            image_flat = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')  # (H * W * 3,)
-            plot = image_flat.reshape(*fig.canvas.get_width_height(), 3)  # (H, W, 3)
+        image_flat = np.frombuffer(canvas.tostring_rgb(), dtype="uint8")  # (H * W * 3,)
+        plot = image_flat.reshape(*fig.canvas.get_width_height(), 3)  # (H, W, 3)
 
         trainer.logger.log_image("Interventions", images=[plot], caption=["intervention"])
         return return_value
