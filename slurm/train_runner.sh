286c008e4003e59c0321b147a0955f8d25f0fdf5
HamidrezaKmK <hamidrezakamkari@gmail.com>
2024-02-06 19:46:12 +0000

    remove train_runner and update sweep

--- slurm/train_runner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ln -sfn /checkpoint/${USER}/${SLURM_JOB_ID} $PWD/runs/${SLURM_JOB_ID}
-
-touch /checkpoint/${USER}/${SLURM_JOB_ID}/DELAYPURGE
-
-. $HOME/pytorch-2-env
-
-python train.py wandb.run_name=${SLURM_JOB_ID} "$@"  # Since list can be in quotes, we need to pass "$@" instead of $@
